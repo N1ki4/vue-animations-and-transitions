e9cc9eea2bc4ef7692a2cae6fce47edd62460ea4
n1ki4 <glazunov_n@ukr.net>
2020-11-16 18:25:53 +0200

    add transitions, and transitions lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition 
+      :css="false" 
+      @enter="enter"
+      @after-enter="afterEnter" 
+      v-on:before-enter="beforeEnter" 
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
+    >
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreDisplayed">Show users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -15,9 +37,80 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { 
+      dialogIsVisible: false, 
+      animatedBlock: false, 
+      paraIsVisible: false,
+      usersAreDisplayed: false,
+      enterInterval: null,
+      leaveInterval: null
+    };
   },
   methods: {
+    enterCancelled(el) {
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
+    afterLeave(el) {
+      console.log("afterLeave");
+      console.log(el);
+    },
+    leave(el, done) {
+      console.log("Leave");
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log("after Enter");
+      console.log(el);
+    },
+    enter(el, done) {
+      console.log("enter");
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave");
+      console.log(el);
+      el.style.opacity = 1;
+    },
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    showUsers() {
+      this.usersAreDisplayed = true
+    },
+    hideUsers() {
+      this.usersAreDisplayed = false
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +150,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /*transition: transform 0.3s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -69,4 +163,41 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
+
+.animate {
+  /* transform: translateX(-50px); */
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+} 
 </style>
